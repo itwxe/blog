2bf64146bc5bbb4bd71478b14dc6242d6685a5c9
itwxe <itwxe@qq.com>
2022-07-30 10:43:46 +0800

    [config] 百度pv统计触发pv build报错修改

--- docs/.vuepress/enhanceApp.ts
+++ docs/.vuepress/enhanceApp.ts
@@ -4,12 +4,13 @@ export default defineClientConfig({
     enhance({ router }) {
         router.beforeEach((to, from, next) => {
             // console.log("切换路由", to.fullPath, from.fullPath);
-            // @ts-ignore
-            const _hmt = window._hmt;
+
             // 触发百度的pv统计
+            // @ts-ignore
             if (typeof _hmt != "undefined") {
                 // 链接中存在#时为锚点链接，不触发pv
                 if (to.path && !to.fullPath.includes("#")) {
+                    // @ts-ignore
                     _hmt.push(["_trackPageview", to.fullPath]);
                     // console.log("上报百度统计", to.fullPath);
                 }
