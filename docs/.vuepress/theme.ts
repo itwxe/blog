d915c2e648bf6bed88bd5b5f5e0292614ee57b63
itwxe <itwxe@qq.com>
2023-05-18 17:17:23 +0800

    config: 开启评论

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -66,28 +66,11 @@ export default hopeTheme({
         // 如果你不需要评论，可以直接删除 comment 配置，
         // 以下配置仅供体验，如果你需要评论，请自行配置并使用自己的环境，详见文档。
         // 为了避免打扰主题开发者以及消耗他的资源，请不要在你的正式环境中直接使用下列配置!!!!!
-        // comment: {
-        //     /**
-        //      * Using Giscus
-        //      */
-        //     // provider: "Giscus",
-        //     // repo: "vuepress-theme-hope/giscus-discussions",
-        //     // repoId: "R_kgDOG_Pt2A",
-        //     // category: "Announcements",
-        //     // categoryId: "DIC_kwDOG_Pt2M4COD69",
-        //
-        //     /**
-        //      * Using Twikoo
-        //      */
-        //     // provider: "Twikoo",
-        //     // envId: "https://twikoo.ccknbc.vercel.app",
-        //
-        //     /**
-        //      * Using Waline
-        //      */
-        //     provider: "Waline",
-        //     serverURL: "https://vuepress-theme-hope-comment.vercel.app",
-        // },
+        comment: {
+            provider: "Artalk",
+            comment: true,
+            server: "https://itwxe.com/comment"
+        },
 
         mdEnhance: {
             align: false,
@@ -98,7 +81,6 @@ export default hopeTheme({
             demo: false,
             echarts: false,
             figure: false,
-            flowchart: false,
             gfm: false,
             imgLazyload: true,
             imgSize: true,
